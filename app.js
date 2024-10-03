c1ce314f225971003abd4c978d2fb8727038b430
Banele Mpofu <banelempofu7979@gmail.com>
2024-10-03 21:09:05 +0200

    Project SetUp: installs dependencies and sets up port environment

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Test");
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(bodyParser.json())
+
+// MYSQL
+
+// LISTEN ON ENVIRONMENT PORT OR 5000
+app.listen(port, () => console.log(`Listen on port ${port}`))
