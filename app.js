6f8ada7ffee5eaf9b4da2e40d34d096b9eb4c43f
mashabelaherold <95069470+mashabelaherold@users.noreply.github.com>
2024-10-09 10:49:56 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,69 +1,19 @@
-var express = require("express")
-var cors = require("cors")
-const router = express.Router();
-var mysql =require('mysql');
-const bcrypt = require('bcrypt')
-const session = require('express-session');
+const express = require('express');
+const userRoutes = require('./src/routes/userRoutes');
 
+const app = express();
+const port = 3000;
 
-var mysqlConnection = require('./config/connection');
-var app=express()
-app.use(express.json())
+// Middleware to parse JSON requests
+app.use(express.json());
 
-app.use(cors());
+// Use the user routes
+app.use('/v1', userRoutes);
 
-//Registration for investor
-router.post('/investor/registration', (req, res) => {
-    const { name, surname, email, password, passwordConfirm } = req.body;
-   
-    if (!strongPass.test(password)) {
-       return res.json({
-           success: false,
-           message: "Please enter a Strong Password"
-       });
-     }
-       mysqlConnection.query('SELECT email FROM investor WHERE email = ?', [email], async (error, results) => {
-           if (error) {
-              console.log(error);
-    }
-    if (results.length > 0) {
-     return res.json({
-     success: false,
-     message: "Email already exists."
-     });
-    } else if (password !== passwordConfirm) {
-     return res.json({
-     success: false,
-     message: "Passwords do not match."
-     });
-    } else {
-     let hashedPassword = await bcrypt.hash(password, 8);
-   
-     mysqlConnection.query('INSERT INTO admin SET ?', { name: name, surname: surname, email: email, password: hashedPassword }, (error, results) => {
-     if (error) {
-      console.log(error);
-     } else {
-      return res.json({
-      success: true,
-      message: "Successfully registered."
-      });
-      console.log(results);
-     }
-     });
-    }
-    });
-   });
+// Use the admin routes
+//app.use('/v1', adminRoutes);
 
-
-
-//connection for server
-app.listen(3001, (err) =>{
-    if (err){
-        console.log(err);
-    }else{
-        console.log('Express server is runnig at port no : 3001');
-    }
-})
-
-
-//console.log("Test");
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
