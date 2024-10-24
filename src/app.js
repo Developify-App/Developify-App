2a2d7cb11e841e03023a151e4e63304d3745e526
Banele Mpofu <banelempofu7979@gmail.com>
2024-10-24 02:32:01 +0200

    API: creates project controller

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const helloRoutes = require('./routes/helloRoutes');
+const bodyParser = require('body-parser');
 
+const helloRoutes = require('./routes/helloRoutes');
 const investorRoutes = require('./routes/investorRoutes');
 const landownerRoutes = require('./routes/landownerRoutes');
+const projectRoutes = require('./routes/projectRoutes');
 
 const app = express();
 
-// Middleware for parsing JSON data
+// Middleware for parsing JSON datas
 app.use(express.json());
 
 // Use the hello routes
@@ -15,5 +17,6 @@ app.use('/api', helloRoutes);
 // Use investor routes
 app.use('/api/investors', investorRoutes);
 app.use('/', landownerRoutes);
+app.use('/', projectRoutes);
 
 module.exports = app;
