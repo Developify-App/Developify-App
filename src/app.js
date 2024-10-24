2f4154116f9e7dbe00c710e9552b80be87f9a8b1
unknown <rammutlaherold@gmail.com>
2024-10-24 11:20:08 -0700

    Update file structure

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express');
 const helloRoutes = require('./routes/helloRoutes');
 const investorRoutes = require('./routes/investorRoutes');
@@ -27,29 +27,4 @@ app.use('/api/account', accountRoutes);
 app.use('/api/transaction', transactionRoutes);
 
 module.exports = app;
-=======
-const express = require('express');
-const helloRoutes = require('./routes/helloRoutes');
-const investorRoutes = require('./routes/investorRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const accountRoutes = require('./routes/accountRoutes');
 
-const app = express();
-
-// Middleware for parsing JSON data
-app.use(express.json());
-
-// Use the hello routes
-app.use('/api', helloRoutes);
-
-// Use investor routes
-app.use('/api/investors', investorRoutes);
-
-// Use admin routes
-app.use('/api/admin', adminRoutes);
-
-// Use account routes
-app.use('/api/account', accountRoutes);
-
-module.exports = app;
->>>>>>> fb5b6ccaa8cbbc78c7bdc8959f9134b105420817
