e1032d7b4210c034548f0c7f388c9b9bbe51259b
mashabelaherold <95069470+mashabelaherold@users.noreply.github.com>
2024-10-30 08:56:36 -0700

    Update accountRoutes.js

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
@@ -14,21 +13,3 @@ router.put('/withdraw', accountController.withdraw);
 
 
 
-=======
-const express = require('express');
-const router = express.Router();
-const accountController = require('../controllers/accountController');
-
-// Define a route that handles post for first deposit
-router.post('/firstDeposit', accountController.firstDeposit);
-
-// Define a route that handles PUT for  deposit
-router.put('/deposit', accountController.deposit);
-
-// Define a route that handles PUT for  withdraw
-router.put('/withdraw', accountController.withdraw);
-
-
-
->>>>>>> fb5b6ccaa8cbbc78c7bdc8959f9134b105420817
-module.exports = router;
